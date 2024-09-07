0e3ca6e2c015abbb45b4bb30bf80183d6175673f
Francesco <goras.francesco@gmail.com>
2024-09-07 20:21:29 +1200

    db helper funcs

--- internal/database/queries.go
+++ internal/database/queries.go
@@ -30,3 +30,59 @@ func UpdateLastSeen(db *sql.DB, id int32) error {
 	_, err := db.Exec(`UPDATE users SET last_seen = $1 WHERE id = $2`, time.Now(), id)
 	return err
 }
+
+func GetFileByName(db *sql.DB, user_id int32, file_name string) (*DBFile, bool, error) {
+	rows, err := db.Query(`SELECT id, user_id, location, file_name, file_size, created_at FROM files WHERE user_id = $1 and file_name = $2`, user_id, file_name)
+	if err != nil {
+		return nil, false, err
+	}
+	if !rows.Next() {
+		return nil, false, nil
+	}
+	defer rows.Close()
+
+	dbFile := &DBFile{}
+	err = rows.Scan(&dbFile.Id, &dbFile.UserId, &dbFile.Location, &dbFile.FileName, &dbFile.FileSize, &dbFile.CreatedAt)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return dbFile, true, nil
+}
+
+func GetLinkByFileId(db *sql.DB, user_id int32, file_id int32) (*DBLink, bool, error) {
+	stmt := `SELECT id, access_key, access_count, file_id, created_by, created_at FROM links WHERE created_by=$1 AND file_id=$2`
+	rows, err := db.Query(stmt, user_id, file_id)
+	if err != nil {
+		return nil, false, err
+	}
+	if !rows.Next() {
+		return nil, false, nil
+	}
+	defer rows.Close()
+
+	dbLink := &DBLink{}
+	err = rows.Scan(&dbLink.Id, &dbLink.AccessKey, &dbLink.AccessCount, &dbLink.FileId, &dbLink.CreatedBy, &dbLink.CreatedAt)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return dbLink, true, nil
+}
+
+type DBFile struct {
+	Id        int32
+	UserId    int32
+	Location  string
+	FileName  string
+	FileSize  int64
+	CreatedAt time.Time
+}
+type DBLink struct {
+	Id          int32
+	AccessKey   string
+	AccessCount int64
+	FileId      int32
+	CreatedBy   int32
+	CreatedAt   time.Time
+}
