ebc0a601b9562b45dba2b768d350a7ef964fbf85
Francesco <goras.francesco@gmail.com>
2024-09-07 17:59:50 +1200

    Add git tag version to binary on build

--- build.ps1
+++ build.ps1
@@ -1,1 +1,5 @@
-$Env:GOOS = "linux"; $Env:GOARCH = "amd64"; $Env:CGO_ENABLED = 0; go build -o dist/storage-api .
+$VERSION = (git describe --tags)
+$Env:GOOS = "linux"; 
+$Env:GOARCH = "amd64"; 
+$Env:CGO_ENABLED = 0; 
+go build -v -ldflags="-X 'main.Version=${VERSION}'" -o dist/storage-api .
