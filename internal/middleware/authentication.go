dd155750a2af2b4bdb1262a648fcab9ad03a1606
Francesco Gorini <goras.francesco@protonmail.com>
2026-02-13 17:26:15 +1300

    Explicit cast & check on session user_id in protected route middleware

--- internal/middleware/authentication.go
+++ internal/middleware/authentication.go
@@ -13,14 +13,13 @@ func Protected(next gin.HandlerFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Check if user has a cookie (authenticated)
 		session := sessions.Default(c)
-		id := session.Get("id")
-		if id == nil {
+		userID, ok := session.Get("id").(int32)
+		if !ok {
 			c.AbortWithError(http.StatusUnauthorized, errors.New("unauthenticated"))
 			return
 		}
 		// Populate request with session values
-		c.Keys["user_id"] = session.Get("id")
-		// TODO: Add more
+		c.Keys["user_id"] = userID
 
 		next(c)
 	}
